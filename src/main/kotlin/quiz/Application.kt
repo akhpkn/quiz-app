ce6f1691472f819c325c94502dcc7934f66aff73
akhpkn <akh.andr@mail.ru>
2021-03-12 00:00:23 +0300

    Added swagger

--- src/main/kotlin/quiz/Application.kt
+++ src/main/kotlin/quiz/Application.kt
@@ -2,8 +2,10 @@ package quiz
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 open class Application
 
 fun main(args: Array<String>) {

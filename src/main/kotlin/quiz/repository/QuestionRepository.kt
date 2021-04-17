09102afa6b6bf619a065644869bcf676b3bd1373
akhpkn <akh.andr@mail.ru>
2021-04-17 23:21:10 +0300

    removed unused method

--- src/main/kotlin/quiz/repository/QuestionRepository.kt
+++ src/main/kotlin/quiz/repository/QuestionRepository.kt
@@ -3,13 +3,9 @@ package quiz.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import quiz.model.Question
-import quiz.model.Quiz
 
 interface QuestionRepository : JpaRepository<Question, Long> {
 
     @Query("select q from Question q join fetch q.quiz where q.id = ?1")
     fun findQuestionById(id: Long): Question?
-
-    @Query("select count(q) from Question q where q.quiz = ?1")
-    fun getQuestionsNumberForQuiz(quiz: Quiz): Int
 }
